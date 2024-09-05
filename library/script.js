ab80b7ef74a536bdae8643568092a8eec11229ac
voxede <7888907+voxede@users.noreply.github.com>
2024-09-05 20:42:48 +0000

    A lot of progress in data presentation

--- library/script.js
+++ library/script.js
@@ -16,18 +16,38 @@ const addBookToLibrary = (title, author, pages, read) => {
 
 addBookToLibrary("myBook", "myAuthor", 1200, true)
 addBookToLibrary("myBook", "myAuthor", 1200, true)
+addBookToLibrary("myBook", "myAuthor", 1200, false)
 addBookToLibrary("myBook", "myAuthor", 1200, true)
-addBookToLibrary("myBook", "myAuthor", 1200, true)
-addBookToLibrary("myBook", "myAuthor", 1200, true)
+addBookToLibrary("myBook", "myAuthor", 1200, false)
 addBookToLibrary("myBook", "myAuthor", 1200, true)
 addBookToLibrary("myBook", "myAuthor", 1200, true)
 
 const showBooks = () => {
     myLibrary.map(book => {
-        let node = document.createElement('h1')
-        let textNode = document.createTextNode(book.title)
-        node.appendChild(textNode)
-        cards.appendChild(node)
+        let card = document.createElement('div')
+        let cardTitle = document.createElement('h1')
+        let cardAuthor = document.createElement('h4')
+        let cardPages = document.createElement('p')
+        let cardRead = document.createElement('p')
+
+        let bookTitle = document.createTextNode(book.title)
+        let bookAuthor = document.createTextNode(`Author: ${book.author}`)
+        let bookPages = document.createTextNode(`Pages: ${book.pages}`)
+        let bookRead = book.read ? document.createTextNode('Read: Yes') : document.createTextNode('Read: Not')
+
+        card.classList.add('card')
+
+        cardTitle.appendChild(bookTitle)
+        cardAuthor.append(bookAuthor)
+        cardPages.append(bookPages)
+        cardRead.append(bookRead)
+
+        card.appendChild(cardTitle)
+        card.appendChild(cardAuthor)
+        card.append(cardPages)
+        card.append(cardRead)
+
+        cards.appendChild(card)
     })
 }
 
